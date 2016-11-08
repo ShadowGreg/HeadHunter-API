c81ccad4122ee0ca8405b506ce493720d9c2623a
Konstantin <konstantin.writing.code@gmail.com>
2016-11-08 09:05:02 +0400

    Add project files.

---
+++ HeadHunter API/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadHunter_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
